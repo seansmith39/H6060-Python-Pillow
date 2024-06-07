40c1171b98a7983ce41d264d8faa330160f186ad
sean.smith <sean.smith@workday.com>
2024-06-07 22:12:40 +0100

    Add Build Logic

--- depends/install_imagequant.sh
+++ depends/install_imagequant.sh
@@ -3,7 +3,7 @@
 
 archive=libimagequant-2.15.1
 
-./download-and-extract.sh $archive https://raw.githubusercontent.com/python-pillow/pillow-depends/master/$archive.tar.gz
+./download-and-extract.sh $archive https://src.fedoraproject.org/lookaside/pkgs/mingw-libimagequant/libimagequant-2.15.1.tar.gz/sha512/56bc8b73859908774ce0a218cfbec81bfb8432c02ddce83880990b5f5ac89ac253ae64ecde43a9664aab3ac69d309bb9ef8ba2201be6c93aacb731b3214ff4b8/libimagequant-2.15.1.tar.gz
 
 pushd $archive
 
